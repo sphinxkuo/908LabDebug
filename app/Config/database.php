b78e47a102bfc6f4bb98c8227a35d3fb57b4659c
Sphinx Kuo <sphinxkuo@gmail.com>
2014-02-18 03:19:13 +0800

    Reset the password.

--- app/Config/database.php
+++ app/Config/database.php
@@ -66,7 +66,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'cakephp',
-		'password' => '1234567',
+		'password' => '12345678',
 		'database' => 'cakephp',
 		'prefix' => 'ck_',
 		'encoding' => 'utf8',
